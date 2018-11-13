1d441bb9b8e9c9f58ca6a0f6bd28193d2b37a6a2
Illia Maksymov <ivashthestampede01@gmail.com>
2018-11-13 11:09:12 +0200

    Реализован функционал добавления тасков

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('task-form');
+const taskInput = document.getElementById('task');
+const taskList = document.querySelector('.collection');
+const filter = document.getElementById('filter');
+const clearBtn = document.querySelector('.clear-tasks');
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+}
+
+loadEventListeners();
+
+function addTask(e) {
+  e.preventDefault();
+
+  const inputValue = taskInput.value,
+        li = document.createElement('li'),
+        text = document.createElement('div'),
+        link = document.createElement('a');
+
+  taskInput.value = '';
+
+  if (inputValue === '') alert('Please add task!'); // Нужно будет доработать
+
+  li.className = 'collection__item';
+
+  text.className = 'collection__item-text';
+  text.textContent = inputValue;
+
+  li.appendChild(text);
+
+  link.className = 'delete-item';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+}
